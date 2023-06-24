464e6564e3272132d1403730c6c48679ae919b98
SOURADIP <souradippanja2015@gmail.com>
2023-06-24 13:35:18 +0530

    delete post query update

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -1,15 +1,20 @@
-<?php 
+<?php
 include "config.php";
-if ($_SESSION['user_role'] == 0) {
-    header("Location: {$hostname}/admin/post.php");
-}
 
 $postid = $_GET['id'];
-$sql = "DELETE FROM post WHERE post_id = {$postid}";
-if(mysqli_query($conn, $sql)){
-header("Location: {$hostname}/admin/post.php");
+$cat_id = $_GET['cat_id'];
+$sql1 = "SELECT * FROM post WHERE post_id = {$postid}";
+$result = mysqli_query($conn, $sql1);
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        unlink("upload/" . $row['post_img']);
+    }
 }
-else{
+
+$sql = "DELETE FROM post WHERE post_id = {$postid};";
+$sql .= "UPDATE category SET post=post-1 WHERE category_id = '{$cat_id}' ";
+if (mysqli_multi_query($conn, $sql)) {
+    header("Location: {$hostname}/admin/post.php");
+} else {
     echo "<P style='color:red; text-align: center; margin: 10px 0;'>SORRY CAN'T DELETE USER {$userid}</p>";
 }
-?>
